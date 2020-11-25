2aa7c9f413bf569b11e020a7711f6ffa0d6bfd46
AsclepiiusUnknown <55474351+AsclepiiusUnknown@users.noreply.github.com>
2020-11-25 20:20:31 +1100

    ReAdded Dynamic Water

---
+++ Assets/Dynamic Water/Scripts/BoxSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    public GameObject boxObject;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject tempBox = Instantiate(boxObject, transform.position, Quaternion.identity);
+            Destroy(tempBox,5);
+        }
+    }
+}
