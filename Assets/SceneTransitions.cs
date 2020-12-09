31c7ff34b332dbca57be334e40a450b0f77a54fc
AsclepiiusUnknown <55474351+AsclepiiusUnknown@users.noreply.github.com>
2020-12-09 20:59:10 +1100

    transition UI

---
+++ Assets/SceneTransitions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public int sceneCount;
+
+    public void NextScene()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex + 1;
+        if (index >= sceneCount)
+            index = 0;
+
+        SceneManager.LoadScene(index);
+    }
+
+    public void PreviousScene()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex - 1;
+        if (index < 0)
+            index = sceneCount - 1;
+
+        SceneManager.LoadScene(index);
+    }
+}
